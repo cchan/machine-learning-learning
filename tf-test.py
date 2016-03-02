4b5d1e7693771b356238b0f83129381b2c731067
Clive Chan <cc@clive.io>
2016-03-02 16:57:40 -0500

    made corrections to aws-setup.sh

--- tf-test.py
+++ tf-test.py
@@ -1,5 +1,11 @@
 import tensorflow as tf
-import input_data
 
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+# Say hello.
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print sess.run(hello)
 
+# Some simple math.
+a = tf.constant(10)
+b = tf.constant(32)
+print sess.run(a+b)
