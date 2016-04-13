8f921baff4504a3b9ffee53b11bea601bf2c641e
Clive Chan <cc@clive.io>
2016-04-13 12:36:18 +0000

    created a two layer network but it's vastly less accurate for some reason

---
+++ tf-mnist-onelayer.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+import input_data
+
+# Load all the data as one-hot
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+# Create a placeholder, filled with feed_dict later
+x = tf.placeholder(tf.float32, [None, 784])
+
+# Create variables which may be changed
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+# Define the single-layer softmax NN using matrix multiplication
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+# Another placeholder, filled with feed_dict later
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+# Cross entropy, a fitness measure, 
+cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
+
+# Let TensorFlow's magic work, gradient-descent-optimizing the cross-entropy.
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+# Start a session and init all its variables
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+
+# Now run the training step, 1000 times, on random 100-element subsets of the dataset
+for i in range(1000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+# Get a vector of whether the one-hot answer matches, for each run.
+# The variable y is filled with a lot of answers from before.
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_, 1))
+
+# Reduce the correct_prediction to an accuracy percent.
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+# Run the "accuracy" op, which will feed the data to the session and make things work.
+print sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+
+
+
--- tf-mnist.py
+++ tf-mnist.py
@@ -1,27 +1,33 @@
 import tensorflow as tf
 import input_data
 
+
 # Load all the data as one-hot
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-# Create a placeholder, filled with feed_dict later
+# Create a placeholder for raw data (image), to be filled with feed_dict later
 x = tf.placeholder(tf.float32, [None, 784])
 
 # Create variables which may be changed
-W = tf.Variable(tf.zeros([784,10]))
-b = tf.Variable(tf.zeros([10]))
+W1 = tf.Variable(tf.zeros([784,100]))
+b1 = tf.Variable(tf.zeros([100]))
+W2 = tf.Variable(tf.zeros([100,10]))
+b2 = tf.Variable(tf.zeros([10]))
 
-# Define the single-layer softmax NN using matrix multiplication
-y = tf.nn.softmax(tf.matmul(x, W) + b)
+# Define the two-layer softmax NN using matrix multiplication
+y = tf.nn.softmax(tf.matmul(x, W1) + b1)
+prediction = tf.nn.softmax(tf.matmul(y, W2) + b2)
 
-# Another placeholder, filled with feed_dict later
-y_ = tf.placeholder(tf.float32, [None, 10])
+# Another placeholder for raw data (class assigned), filled with feed_dict later
+label = tf.placeholder(tf.float32, [None, 10])
 
-# Cross entropy, a fitness measure, 
-cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
+# fitness measure
+cost = tf.reduce_mean(tf.square(label- prediction))
 
-# Let TensorFlow's magic work, gradient-descent-optimizing the cross-entropy.
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+# Let TensorFlow's magic work, gradient-descent-minimizing the cost.
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
+
+
 
 # Start a session and init all its variables
 init = tf.initialize_all_variables()
@@ -31,17 +37,15 @@ sess.run(init)
 # Now run the training step, 1000 times, on random 100-element subsets of the dataset
 for i in range(1000):
   batch_xs, batch_ys = mnist.train.next_batch(100)
-  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+  sess.run(train_step, feed_dict={x: batch_xs, label: batch_ys})
 
 # Get a vector of whether the one-hot answer matches, for each run.
-# The variable y is filled with a lot of answers from before.
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_, 1))
+# The variable prediction is filled with a lot of answers from before.
+correct_prediction = tf.equal(tf.argmax(prediction,1), tf.argmax(label, 1))
 
 # Reduce the correct_prediction to an accuracy percent.
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
 # Run the "accuracy" op, which will feed the data to the session and make things work.
-print sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
-
-
+print sess.run(accuracy, feed_dict={x: mnist.test.images, label: mnist.test.labels})
 
